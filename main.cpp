12c48100ccc0f114b08c3e5723df3232a94ff33b
Nestorhn91 <Nestor.lagos@unitec.edu>
2014-08-13 09:50:46 -0600

    Examen_1
    este fue el examen mas falta que hice me siento que no fui capas de
    realisarlo 100%

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include<queue>
+
+
+using namespace std;
+
+
+// LO SIENTO ING PERO NO ESTA COMPLETO SIENDO FRANCO NO ESTUDIE AL 100% ASI QUE PERDI ESTOS PUNTOS LO SIENTO
+//VOY A TERNER QUE PRACTICAR MAS POR NO ME SENTI CAPAS DE REALISAR ESTE EXAMENN
+
+cout<<"Ejercicio getTamano:\t\t";
+if(getTamano(mi_set_str)==3 && getTamano(mi_set_str2)==2)
+{
+nota++;
+cout<<"Correcto"<<endl;
+}else
+{
+//cout<<"Incorrecto"<<endl;
+}
+int main()
+{// Agrega valor al Set.
+
+ /*void agregar(set<int> *mi_set,int valor)
+ {
+   mi_set -> insert (valor);
+
+ }
+// Devuelve verdadero si valor existe en el Set, de lo contrario devuelve falso.
+
+bool existe(int valor)
+{
+    while (valor.empty ()==false)
+{
+if (valor.front()==str)
+{
+return true;
+}
+valor.pop();
+}
+return false;
+
+}*/
+
+
+//cout<<"Incorrecto"<<endl;
+}
+
+int getTamano(queue<string> mi_set)
+{
+   return mi_set.size();
+}
+
+/*int getNumeroMayor()
+{
+    int mayor=-9999;
+while(mi_cola.empty()==false)
+{
+if(mi_cola.front()>mayor)
+{
+mayor=mi_cola.front();
+}
+mi_cola.pop();
+}
+return mayor;
+
+}
+// Devuelve todos los elementos del Set contenidos en un vector.
+
+std::vector<int> convertirAVector()
+{
+
+}*/
+
+
+
